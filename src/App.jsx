1565da3cc82a3df81d77e000dcbb468019ac91b7
Affaque Ahmad <affaque109@gmail.com>
2025-06-08 15:05:53 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import TodoList from "./components/TodoList";
 
 export default function App() {
-
-
-
   return (
-    <div  className="flex h-screen items-center justify-center overflow-hidden" >
-
-      <div className="w-1/2" >
-       <img 
-          src="/pic5.jpg" 
-          alt="Todo Visual" 
-          className="w-full h-full object-cover" 
+    <div className="flex flex-col lg:flex-row h-screen">
+      
+      {/* Left: Image */}
+      <div className="w-full lg:w-1/2 h-64 lg:h-full">
+        <img
+          src="/pic5.jpg"
+          alt="Todo Visual"
+          className="w-full h-full object-cover"
         />
-
       </div>
 
-      <div className="w-1/2 flex justify-center items-center " >
-      <TodoList/>
-
+      {/* Right: Todo List */}
+      <div className="w-full lg:w-1/2 flex justify-center items-center p-4 ">
+        <TodoList />
       </div>
-
+      
     </div>
-    
-  )
+  );
 }
-
-
